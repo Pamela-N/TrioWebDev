27812e700db588855c936d014f7a547a74c8e825
keabe <keabetswemogotsi@gmail.com>
2020-08-05 16:23:10 +0200

    changes made

--- js/myScript.js
+++ js/myScript.js
@@ -30,21 +30,6 @@
 // });
 
 
-const recipeBtn = document.querySelector('.recipe');
-const fullRec = document.querySelector('.fullRecipe');
-const hide = document.querySelector('.exit');
-
-fullRec.style.display = 'none';
-
-recipeBtn.addEventListener('click', () => {
-    recipeBtn.style.display = 'none';
-    fullRec.style.display = 'block';
-});
-
-fullRec.addEventListener('click', () => {
-    recipeBtn.style.display = 'block';
-    fullRec.style.display = 'none';
-});
                          
 const show = document.querySelector('.ReadMore');
 const butto = document.querySelector('.but');
