32f59977641875b0162f6d9a24f63dd4d07044ec
Pamela <60347329+Pamela-N@users.noreply.github.com>
2020-08-06 16:39:54 +0200

    Update myScript.js

--- js/myScript.js
+++ js/myScript.js
@@ -21,16 +21,15 @@ fullRec.addEventListener('click', () => {
 });
 
 // End of Community section
-// About Section functions 
-// This function make the full information on the about page display when you click on the read more button
+
 butto.addEventListener('click', () => {
     butto.style.display = 'none';
     show.style.display = 'block';
 });
 
 show.addEventListener('click', () => {
-    butto.style.display = 'block';
     show.style.display = 'none';
+    butto.style.display = 'block';
 });
 
 // Get Involved
