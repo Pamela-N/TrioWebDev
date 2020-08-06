58d75bf0cfc9a7a56b4ef326c194c42acc3dbe41
keabe <keabetswemogotsi@gmail.com>
2020-08-06 11:56:25 +0200

    conflicts

--- js/myScript.js
+++ js/myScript.js
@@ -1,33 +1,7 @@
-<<<<<<< HEAD
 // Community section
 const recipeBtn = document.querySelector('.recipe');
 const fullRec = document.querySelector('.fullRecipe');
 const hide = document.querySelector('.exit');
-=======
-
-// const whole = document.querySelector('.involved');
-// const donate = document.querySelector('.fa-shopping-basket');
-// const banking = document.querySelector('.details');
-// const exit = document.querySelector('.close');
-// const reg = document.querySelector('.register');
-// const male = document.querySelector('.fa-male');
-
-
-// banking.style.display = 'none';
-// reg.style.display = 'none';
-
-
-// donate.addEventListener('click', () => {
-//     whole.style.display = 'none';
-//     banking.style.display = 'block';
-// });
-
-
-// male.addEventListener('click', () => {
-//     whole.style.display = 'none';
-//     reg.style.display = 'block';
-// });
->>>>>>> 98650bbb7f7fc39d0b9053525063a810d98a36a7
 
 fullRec.style.display = 'none';
 
