58aae0d429fb928fb531434dc8965267a028f3e3
keabe <keabetswemogotsi@gmail.com>
2020-08-06 11:41:32 +0200

    Changes to the community made

--- js/myScript.js
+++ js/myScript.js
@@ -1,33 +1,19 @@
-// const whole = document.querySelector('.involved');
-// const donate = document.querySelector('.fa-shopping-basket');
-// const banking = document.querySelector('.details');
-// const exit = document.querySelector('.close');
-// const reg = document.querySelector('.register');
-// const male = document.querySelector('.fa-male');
+// Community section
+const recipeBtn = document.querySelector('.recipe');
+const fullRec = document.querySelector('.fullRecipe');
+const hide = document.querySelector('.exit');
 
+fullRec.style.display = 'none';
 
-// banking.style.display = 'none';
-// reg.style.display = 'none';
+recipeBtn.addEventListener('click', () => {
+    recipeBtn.style.display = 'none';
+    fullRec.style.display = 'block';
+});
 
-
-// donate.addEventListener('click', () => {
-//     whole.style.display = 'none';
-//     banking.style.display = 'block';
-// });
-
-
-// male.addEventListener('click', () => {
-//     whole.style.display = 'none';
-//     reg.style.display = 'block';
-// });
-
-
-
-// exit.addEventListener('click', () => {
-//     banking.style.display = 'none';
-//     reg.style.display = 'none';
-//     whole.style.display = 'block';
-// });
+fullRec.addEventListener('click', () => {
+    recipeBtn.style.display = 'block';
+    fullRec.style.display = 'none';
+});
 
 
                          
